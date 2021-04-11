170abb7566f3dd843f3e2fed3e5715b9831a00b1
Shyren <shyren.more30@gmail.com>
2021-04-11 14:23:36 +0530

    Add article section for self-help & motivation 📦

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -3,91 +3,80 @@ import { useFetch } from "../Hooks/UseFetch";
 import '../Styles/articles.css'
 const Articles = () => {
 
-    const [productive, setProductive] = useState([])
-    const [selfhelp, setSelfhelp] = useState([])
-    const [motivation, setMotivation] = useState([])
-
     const [data, setData] = useState([])
-
-    const [loading1, isLoading1] = useState(true)
-    const [loading2, isLoading2] = useState(true)
-    const [loading3, isLoading3] = useState(true)
-    const display = productive;
+    const [loading, isLoading] = useState(true)
+    // const display = productive;
     const url1 = 'https://hn.algolia.com/api/v1/search?query=productivity&page=0';
     const url2 = 'https://hn.algolia.com/api/v1/search?query=selfhelp&page=0';
     const url3 = 'https://hn.algolia.com/api/v1/search?query=motivation&page=0';
     
+    const fetchSingle = async () => {
+        try {
+            const response = await fetch(url1)
+            const data = await response.json()
+            setData(data.hits)
+            isLoading(false)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const fetchData = async (url) => {
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            setData(data.hits)
+            isLoading(false)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     
-    const fetchProductivity = async (url) => {
-        // isLoading1(true)
-        try {
-            const response = await fetch(url)
-            const data = await response.json()
-            setProductive(data.hits)
-            isLoading1(false)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    const fetchSelfHelp = async (url) => {
-        try {
-            const response = await fetch(url)
-            const data = await response.json()
-            setSelfhelp(data.hits)
-            isLoading2(false)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    const fetchMotivation = async (url) => {
-        try {
-            const response = await fetch(url)
-            const data = await response.json()
-            setMotivation(data.hits)
-            isLoading3(false)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     useEffect(() => {
-        fetchProductivity(url1)
-        fetchSelfHelp(url2)
-        fetchMotivation(url3)
-    }, [url1, url2, url3, display])
+        fetchSingle()
+    }, [])
 
     
-    if (loading1 || loading2 || loading3)
+    if (loading)
     {
         return<div className="loading"></div>
     }
     return (
         <>
-            <header className="header text-center my-4 py-4">
-                <div className="heading_text">
+            <header className="header text-center mt-4 pt-4">
+                <div>
                     <h3>Articles</h3>
+                    <div className="underline mx-auto"></div>
                 </div>
             </header>
             {/* {console.log(productive)} */}
-
+            
             <div className="btn-container">
-                <button className="filter-btn">Productivity</button>
-                <button className="filter-btn" onClick={()=>{fetchProductivity('https://hn.algolia.com/api/v1/search?query=selfhelp&page=0')}}>Self-Help</button>
-                <button className="filter-btn">Motivation</button>
+                <button className="filter-btn" onClick={() => {
+                    fetchData(url1)
+                    isLoading(true)
+                }}>Productivity</button>
+                <button className="filter-btn" onClick={() => {
+                    fetchData(url2)
+                    isLoading(true)
+                }}>Self-Help</button>
+                <button className="filter-btn" onClick={() => {
+                    fetchData(url3)
+                    isLoading(true)
+                }}>Motivation</button>
             </div>
-
+            <div className="mega_wrapper">
             <section className="stories">
                 {
-                    productive.map((story) => {
+                    data.map((story) => {
                     const { objectID, title, num_comments, url, author } = story;
                     
                         return (
                             <article className="story" key={objectID}>
                                 <h4 className="title">{title}</h4>
                                 <p className="info">
-                                    <span>{author} | </span> {num_comments}
+                                    <span>{author} | </span> {num_comments} {' '}
                                     comments
                                 </p>
                                 <a
@@ -102,7 +91,8 @@ const Articles = () => {
                         )
                     })
                 }
-            </section>
+                </section>
+            </div>
         </>
     )
 }
