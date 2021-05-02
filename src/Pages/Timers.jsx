f414cc3e50604430d71e19ac0d2a56f8a656e5ed
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-05-02 17:28:51 +0530

    Timer page is initialized

--- src/Pages/Timers.jsx
+++ src/Pages/Timers.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import styles from '../Styles/timer.module.css'
 
-const Timers = () => {
+function Timers() {
+    let countdown;
+    let timeremaining = 60;
+    function startCountdown(e){
+        console.log('starts');
+        countdown = setInterval(function(){
+            timeremaining -= 1;
+                
+            if(timeremaining <= 0) {
+                clearInterval(countdown); 
+                console.log("stop");                                               
+                
+                // as timer is over we are not playing 
+                
+            }
+            }, 1000); 
+    }
+     
     return (
         <div>
             <div id="notes-body">
                 <div className="container">
                     <div className={styles.center}>
                     <h2>TIMERS</h2>    
+                    <button onClick={startCountdown}>AAA</button>
                     </div>
+                    
                 </div>
                 
             </div>
