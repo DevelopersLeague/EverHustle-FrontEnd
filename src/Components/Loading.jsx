bbf5475dbb9a360d96c6a0be5609a29fb35d30e6
Shyren <shyren.more30@gmail.com>
2021-05-06 23:46:18 +0530

    Add loading component

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Styles/articles.css'
+const Loading = () => {
+    return (
+        <div className="loading">
+        </div>
+    )
+}
+
+export default Loading
